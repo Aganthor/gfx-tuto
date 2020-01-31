8352f6ad3512dd59ae4717571a4b604b9e67030b
Aganthor <psyluche@gmail.com>
2020-01-31 09:15:19 -0500

    Initial commit!

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use] extern crate gfx;
+
+extern crate gfx_window_glutin;
+extern crate glutin;
+
+use gfx::traits::FactoryExt;
+use gfx::Device;
+use gfx_window_glutin as gfx_glutin;
+
+pub type ColorFormat = gfx::format::Srgba8;
+pub type DepthFormat = gfx::format::DepthStencil;
+
+const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+
+pub fn main() {
+    
+}
